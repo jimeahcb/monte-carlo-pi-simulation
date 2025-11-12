c4adcdc8f9e97abb2c7a27f4d6b32955406bb0a9
Jimeah <hello@jimeahclariz.com>
2025-11-11 22:22:04 -0800

    commit small changes to fix git push

--- pi.py
+++ pi.py
@@ -5,6 +5,8 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.patches import Wedge
+
 
 # creates the function that approximates pi given the number of points
 def pi_approximation(num_points):
@@ -55,7 +57,44 @@ canvas.get_tk_widget().pack(pady=10)
 
 # function to start the simulation
 def start_simulation():
+    ax.clear()
 
+    # draw quarter of a circle using matplotlib function
+    ax.add_patch(Wedge((0, 0), 1, 0, 90, fill=False, edgecolor='blue'))
+
+    # grab number of points from user with error catching
+    try:
+        total = int(entry.get())
+        if total < 1 or total > 10001:
+            raise ValueError
+    except:
+        messagebox.showerror("Invalid value!", "Please enter an integer from 1 to 10,000.")
+        return
+
+    window.title("Drawing... Please Wait")
+    window.update()
+
+    # make random points (x,y) based on user input
+    inside = 0
+    x = np.random.rand(total)
+    y = np.random.rand(total)
+
+    # tally points inside quarter circle
+    inside = x**2 + y**2 <= 1
+
+    # plot all points with color coding
+    ax.scatter(x[inside], y[inside], color='g', s=2)
+    ax.scatter(x[~inside], y[~inside], color='r', s=2)
+
+    # compute pi and present graph 
+    pi_est = 4 * np.sum(inside) / total
+    ax.set_title(f"Pi ≈ {pi_est:.6f}")
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    canvas.draw() 
+    window.title("Pi Approximation Done")
+
+# function to reset the simulation clearing drawings 
 def reset_simulation():
     window.title("Pi Approximation Simulation using Monte Carlo Method")
     window.update()  # makes the title text change immediately
